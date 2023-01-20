32f5703c81c2f1ca6ebbbdf7a47a12c0ad348680
Septian Maulana <septianm028@gmail.com>
2023-01-20 23:08:46 +0700

    feat: example of fetching user input (two-way binding)

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./NewGoal.css";
 
 const NewGoal = (props) => {
+  const [enteredText, setEnteredText] = useState("");
+
   const addGoalHandler = (e) => {
     e.preventDefault();
 
     const newGoal = {
       id: Math.floor(Math.random() * 10000000).toString(),
-      text: "My new goal",
+      text: enteredText,
     };
 
     props.onAddGoal(newGoal);
+    setEnteredText("");
+  };
+
+  const textChangeHandler = (e) => {
+    setEnteredText(e.target.value);
   };
 
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
+      <input type="text" value={enteredText} onChange={textChangeHandler} />
       <button type="submit">Add Goal</button>
     </form>
   );
